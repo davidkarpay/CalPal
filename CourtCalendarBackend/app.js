ccb71b2c2cea6a521155d678da43320c2e0ebf17
twidderbot <dlkarpay@gmail.com>
2025-04-21 16:21:34 -0400

    Initial Commit

---
+++ CourtCalendarBackend/app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+app.use(express.json()); // Middleware to parse JSON request bodies
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get('/', (req, res) => res.send('Server is running'));
